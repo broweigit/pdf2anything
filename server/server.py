22617e7c3e32ef9d25f5cb304f728d912a6eb8a7
browei <12010524@mail.sustech.edu.cn>
2023-05-17 16:02:53 +0800

    add ant design icons to packet.json

--- server/server.py
+++ server/server.py
@@ -9,20 +9,15 @@ def create_app():
     app.config['SECRET_KEY'] = '12010524'
     CORS(app)
 
-    # 鍒濆鍖朞CRSystem
     with app.app_context():
         current_app.ocr = OCRSystem()
-
-    # ...鍏朵粬閰嶇疆鍜岃矾鐢�
 
     @app.route('/upload-image', methods=['POST'])
     def upload_image():
         with app.app_context():
 
-            # 浠� POST 璇锋眰涓幏鍙栧浘鍍忔枃浠�
             img_file = request.files['image']
             
-            # 灏嗗浘鍍忔枃浠惰鍏ヤ负 OpenCV 瀵硅薄
             img_array = np.fromstring(img_file.read(), np.uint8)
             current_app.img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
@@ -36,10 +31,9 @@ def create_app():
     
     @app.route('/table-extract', methods=['GET'])
     def get_table_extract():
-        bbox = request.args.get('bbox') # 浠� GET 璇锋眰鐨勫弬鏁颁腑鑾峰彇 bbox 鍊�
-        bbox = [int(i) for i in bbox[1:-1].split(',')] # 灏� bbox 瀛楃涓茶浆鎹负鏁扮粍
+        bbox = request.args.get('bbox')
+        bbox = [int(i) for i in bbox[1:-1].split(',')]
 
-        # 鎴彇鐩稿簲閮ㄥ垎鐨凴OI
         x, y, w, h = bbox
         img_roi = current_app.img[y:y+h, x:x+w]
 
@@ -53,11 +47,10 @@ def create_app():
     
     @app.route('/text-extract', methods=['GET'])
     def get_text_extract():
-        bbox = request.args.get('bbox') # 浠� GET 璇锋眰鐨勫弬鏁颁腑鑾峰彇 bbox 鍊�
-        lang = request.args.get('lang') # 浠� GET 璇锋眰鐨勫弬鏁颁腑鑾峰彇 bbox 鍊�
-        bbox = [int(i) for i in bbox[1:-1].split(',')] # 灏� bbox 瀛楃涓茶浆鎹负鏁扮粍
+        bbox = request.args.get('bbox') 
+        lang = request.args.get('lang') 
+        bbox = [int(i) for i in bbox[1:-1].split(',')] 
 
-        # 鎴彇鐩稿簲閮ㄥ垎鐨凴OI
         x, y, w, h = bbox
         img_roi = current_app.img[y:y+h, x:x+w]
 
